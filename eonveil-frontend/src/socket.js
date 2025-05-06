fa1dabafe44690309349fb1d290420bc92d213b7
Cody Keith <codykeith9@gmail.com>
2025-05-05 21:34:42 -0400

    Updated title page corrected notes

--- eonveil-frontend/src/socket.js
+++ eonveil-frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // This connects to your backend server
+const socket = io("http://localhost:3000"); // This connects to the backend server
 
 export default socket;
